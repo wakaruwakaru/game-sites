926bb6271ffd45f11834c9c9d99fd363841ce0b2
wakaruwakaru <hayahayahayahaya2345@gmail.com>
2025-12-25 00:27:06 +0900

    Update main2.js

--- chat/chat0001/js/main2.js
+++ chat/chat0001/js/main2.js
@@ -664,7 +664,7 @@ function getReadCountExcludingMe(msgTime){
   localStorage.setItem("account1", "");
   if((token1 !== localStorage.getItem("key1")) || (token3 == null)){
     localStorage.setItem("key1", "unauthorized");
-    localStorage.setItem("requestPage1", "chat/chat0001/chat0001-2");
+    localStorage.setItem("requestPage1", "chat/chat0001/chat0001-3");
     location.href = "/game-sites/";
   }else{
     localStorage.setItem("key1", "");
@@ -676,7 +676,7 @@ function getReadCountExcludingMe(msgTime){
     const key1 = crypto.randomUUID();
     localStorage.setItem("key1", key1);
     localStorage.setItem("account1", token3);
-    location.href = "/game-sites/chat/chat0001/chat0001-2?key1=" + key1;
+    location.href = "/game-sites/chat/chat0001/chat0001-3?key1=" + key1;
   }
   function backPage(){
     const key1 = crypto.randomUUID();
