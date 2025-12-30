8cbcf872d3698f79c3b205106e6d7b7ab939ee6a
wakaruwakaru <hayahayahayahaya2345@gmail.com>
2025-12-30 18:42:15 +0900

    Fix stamp image list range for koizumi

--- chat/chat0001/js/main.js
+++ chat/chat0001/js/main.js
@@ -47,7 +47,7 @@ function updateMediaContent(type){
     renderEmojiList();
   }else if(type == "stamp"){
     renderImgList("stamp", "genshin", 1, 9, "png");
-    renderImgList("stamp", "koizumi", 1, 15, "png");
+    renderImgList("stamp", "koizumi", 1, 14, "png");
     renderImgList("stamp", "other", 1, 17, "png");
   }else if(type == "gif"){
     renderImgList("gif", "genshin", 1, 8, "gif");
