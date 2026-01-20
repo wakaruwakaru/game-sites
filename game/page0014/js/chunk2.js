366c5be2269472365ff451b470fd509ccd13c107
wakaruwakaru <hayahayahayahaya2345@gmail.com>
2026-01-20 21:15:27 +0900

    Update chunk2.js

--- game/page0014/js/chunk2.js
+++ game/page0014/js/chunk2.js
@@ -1,3 +1,7 @@
+const terrainWorker = new Worker(
+  './terrain.worker.js',
+  { type: 'module' }
+)
 (this.webpackJsonp = this.webpackJsonp || []).push([[0], Array(38).concat([function(e, n, t) {
     e.exports = t.p + "jpg/Rock_2.jpg"
 }
